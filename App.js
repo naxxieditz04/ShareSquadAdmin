92e3332d85b755f69a122f7016992740df6b8003
naxxieditz04 <nazibulk04@gmail.com>
2025-08-02 15:55:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import Login from './components/Login';
+import Dashboard from './pages/Dashboard';
+import Links from './pages/Links';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/links" element={<PrivateRoute><Links /></PrivateRoute>} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
