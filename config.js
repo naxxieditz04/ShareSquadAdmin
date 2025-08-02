c5092528ac81e27eb74ce1becb0f09215d1cad7d
naxxieditz04 <nazibulk04@gmail.com>
2025-08-02 16:27:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBCwoxp278aud76u5pFE7mkqfiQL2T8S0s",
   authDomain: "grouplinkapp.firebaseapp.com",
+  databaseURL: "https://grouplinkapp-default-rtdb.firebaseio.com",
   projectId: "grouplinkapp",
   storageBucket: "grouplinkapp.firebasestorage.app",
   messagingSenderId: "543518481244",
-  appId: "1:543518481244:web:4d5c5a66ea214ab4e67f99"
+  appId: "1:543518481244:web:4d5c5a66ea214ab4e67f99",
+  measurementId: "G-K4NL2Y0XYC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+const analytics = getAnalytics(app);
